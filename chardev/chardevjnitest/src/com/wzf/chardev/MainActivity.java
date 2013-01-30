882e5b1039a1246c3e42fd87864eec1be6e1e8a5
wuzhifeng <zhifeng_wu@netcom-hk.com>
2013-01-30 15:06:05 +0800

    apk中加入jni调用，设置两个按钮用于读写chardev设备

--- chardev/chardevjnitest/src/com/wzf/chardev/MainActivity.java
+++ chardev/chardevjnitest/src/com/wzf/chardev/MainActivity.java
@@ -1,16 +1,50 @@
 
 package com.wzf.chardev;
 
+import com.wzf.chardev.chardevclass;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.util.Log;
+
+import android.view.View; 
+import android.view.View.OnClickListener; 
+import android.widget.Button; 
+import android.widget.TextView; 
 
 public class MainActivity extends Activity {
-
+    private static final String TAG = "chardevapk";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        OnClickListener list1=new OnClickListener(){
+            public void onClick(View v)
+            {
+                chardevclass chardevtest = new chardevclass();
+                Log.i(TAG, "chardev apk read ");
+                chardevtest.read();
+                TextView text=(TextView)findViewById(R.id.textView1);
+                CharSequence t=text.getText();
+                text.setText(t+"\nRead");
+            }
+           };
+           Button read=(Button)findViewById(R.id.button1);
+           read.setOnClickListener(list1);
+           OnClickListener list2=new OnClickListener(){
+               public void onClick(View v)
+               {
+                   chardevclass chardevtest = new chardevclass();
+                   Log.i(TAG, "chardev apk write ");
+                   chardevtest.write(8888);
+                   TextView text=(TextView)findViewById(R.id.textView1);
+                   CharSequence t=text.getText();
+                   text.setText(t+"\nWrite");
+               }
+              };
+              Button write=(Button)findViewById(R.id.button2);
+              write.setOnClickListener(list2);
     }
 
     @Override
