3ab5bf705bf870feecca005dc5ebd3dd432e81f6
wuzhifeng <zhifeng_wu@netcom-hk.com>
2013-01-30 14:22:48 +0800

    修改Android.mk文件，可以将so库打包到apk中
    注：
    使用Eclipse build APK文件,只要将so文件放在与src平级的地方创建目录结构libs/armeabi,文件名格式如:libs/armeabi/libxxx.so.
    eclipse在打包的时候会根据文件名的命名规则（lib****.so）去打包so文件，开头和结尾必须分别为“lib”和“.so”，否则是不会打包到apk文件中的

--- chardev/chardevjnitest/Android.mk
+++ chardev/chardevjnitest/Android.mk
@@ -7,8 +7,11 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := chardevapk
 LOCAL_CERTIFICATE := platform
+LOCAL_REQUIRED_MODULES := libchardevjni
+LOCAL_JNI_SHARED_LIBRARIES := libchardevjni
 
 include $(BUILD_PACKAGE)
+include $(LOCAL_PATH)/jni/Android.mk
 
 # Use the folloing include to make our test apk.
 include $(call all-makefiles-under,$(LOCAL_PATH))
